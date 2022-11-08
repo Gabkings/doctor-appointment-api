2b8c4a0261e87a84d66e25466aa7c0affe45c3a8
Gabriel Gitonga <gabworks51@gmail.com>
2022-11-08 09:19:55 +0300

    Adding login

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express')
 const { default: connectDB } = require('./config/db')
 require('dotenv').config()
 const app = express()
+
 const db = require('./config/db')
 db()
+const userRoutes = require('./routes/userRoutes')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/user', userRoutes)
 const port = process.env.PORT | 5000
 
 app.listen(port, () => console.log('Server running at port ' + port))
