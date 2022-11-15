a8c9d7c486c2e592860a423a081e6d263b8f187b
Gabriel Gitonga <gabworks51@gmail.com>
2022-11-16 01:33:10 +0300

    Adding user middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
+const authMiddleware = require('../middlewares/authMiddleWare')
 
 
 route.post("/login", async(req, res) => {
@@ -57,6 +58,27 @@ route.post("/register", async(req, res) => {
         res
             .status(500)
             .send({ message: error.message, success: false, error });
+    }
+});
+
+route.post("/get-user-info-by-id", authMiddleware, async(req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.body.userId });
+        user.password = undefined;
+        if (!user) {
+            return res
+                .status(200)
+                .send({ message: "User does not exist", success: false });
+        } else {
+            res.status(200).send({
+                success: true,
+                data: user,
+            });
+        }
+    } catch (error) {
+        res
+            .status(500)
+            .send({ message: "Error getting user info", success: false, error });
     }
 });
 
