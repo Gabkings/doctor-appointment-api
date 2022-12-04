695fd47cb5db1bcdf19403f77388317297857c7c
Gabriel Gitonga <gabworks51@gmail.com>
2022-12-04 11:13:05 +0300

    Adding connection to atlas online cluster

--- config/db.js
+++ config/db.js
@@ -6,19 +6,28 @@ dotenv.config();
 
 const url = process.env.MONGO_URL;
 
-async function connectDB() {
-    await mongoose
-        .connect(url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log("Connected to the database!");
-        })
-        .catch((err) => {
-            console.log("Cannot connect to the database!", err);
-            process.exit();
-        });
+// async function connectDB() {
+//     await mongoose
+//         .connect(url, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         })
+//         .then(() => {
+//             console.log("Connected to the database!");
+//         })
+//         .catch((err) => {
+//             console.log("Cannot connect to the database!", err);
+//             process.exit();
+//         });
+// }
+
+
+const connectDB = async() => {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log(`Database Connected  hosted by ${conn.connection.host}`)
 }
 
 module.exports = connectDB;
